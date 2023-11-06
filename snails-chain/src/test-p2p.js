9f5935249273013a624051d6043b44151423bb8d
twiglau <twigliu@hotmail.com>
2023-11-06 18:15:09 +0800

    测试p2p

---
+++ snails-chain/src/test-p2p.js
@@ -0,0 +1,23 @@
+const dgram = require("dgram");
+const udp = dgram.createSocket("udp4");
+
+// 收信息
+udp.on("message", (data, remote) => {
+  console.log("accept message:", data.toString());
+});
+udp.on("listening", function () {
+  const addrInfo = udp.address();
+  console.log("udp server is listening: ", addrInfo.address, addrInfo.port);
+});
+
+udp.bind(8002);
+
+// 发送信息
+function send(message, port, host) {
+  console.log("send message", message, port, host);
+  udp.send(Buffer.from(message), port, host);
+}
+
+// 判断命令行有没有输入 port 和 host
+const port = process.argv[2];
+const host = process.argv[3];
